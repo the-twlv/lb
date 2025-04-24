65ea90f2f4f6f82fc1725937ffa8cbbb6a5b1f9a
the-twlv <jobmusembim@gmail.com>
2025-04-24 21:13:04 +0300

    Remove SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 // src/pages/index.js
 import * as React from "react"
-import Seo from "../components/seo"
 import Home from "../components/Home"
 
-export default function IndexPage() {
-  return (
-    <>
-      <Seo
-        title="Liya Blogs"
-        description="Lifestyle Blogger & Media Creative"
-        image="/liya-cover.jpg"
-      />
-      <Home />
-    </>
-  )
+const IndexPage = () => {
+  return <Home />
 }
+
+export default IndexPage
