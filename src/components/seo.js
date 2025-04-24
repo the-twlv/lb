14109bc5197f31a509110125590315587e86e46e
the-twlv <jobmusembim@gmail.com>
2025-04-24 13:34:25 +0300

    Seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,48 +1,14 @@
-import * as React from "react"
+// src/components/SEO.js
+import React from "react"
 import { Helmet } from "react-helmet"
 
-export default function SEO({
-  title = "Liya Blogs",
-  description = "Lifestyle Blogger & Media Creative",
-  lang = "en",
-  meta = [],
-}) {
+const SEO = ({ title }) => {
   return (
-    <Helmet
-      htmlAttributes={{ lang }}
-      title={title}
-      meta={[
-        {
-          name: `description`,
-          content: description,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: description,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: description,
-        },
-      ].concat(meta)}
-    >
-      <link rel="icon" href="/favicon.png" />
+    <Helmet>
+      <title>{title}</title>
+      <link rel="icon" href="/favicon.png" type="image/png" />
     </Helmet>
   )
 }
+
+export default SEO
